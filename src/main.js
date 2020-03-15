d02f43aec2f56cd5da8ec7c338c946f8f1e850e1
Aimer <Amier@qq.com>
2020-03-15 14:22:02 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
+
+// 全局注册  实际上调用了 Vant导出的对象的方法=>install方法
+Vue.use(Vant)
 
 new Vue({
   router,
